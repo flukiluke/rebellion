c683a427e8470d8d5a36923a4f5a6e91dbf3d2ab
Nir Palombo <>
2019-05-23 17:29:08 +1000

    Parse command line arguments -- moved the function to the Configuration class.

--- src/com/swen90004/rebellion/Configuration.java
+++ src/com/swen90004/rebellion/Configuration.java
@@ -4,10 +4,13 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Configuration {
     private static final String CONFIG_FILE = "config.properties";
-    static Properties properties = new Properties();
+    private static Properties properties = new Properties();
 
     public static void loadConfiguration() {
         try {
@@ -41,5 +44,39 @@ public class Configuration {
             throw new Error("Property " + key + " not defined");
         }
         return Boolean.parseBoolean(raw);
+    }
+
+    /** Read command line and override any configuration values.
+     *
+     * @param args The args array passed to main()
+     */
+    public static void parseCmdLineArgs(String[] args) {
+        for (String arg: args) {
+            if (arg.equals("--help") || arg.equals("-h")) {
+                System.out.println("Here are the default values of the program:");
+                Set<String> keys = Configuration.properties.stringPropertyNames();
+                for (String key : keys) {
+                    System.out.println("  " + key + " = " + Configuration.properties.getProperty(key));
+                }
+                System.out.println("It can be executed with different values for example:");
+                System.out.println("java Simulation --k=2.5 --vision=5");
+                System.exit(0);
+            }
+        }
+
+        // Expect a series of --x=y arguments
+        Pattern pattern = Pattern.compile("^--(.+)=(.*)$");
+        for (String arg : args) {
+            Matcher match = pattern.matcher(arg);
+            if (!match.matches()) {
+                System.out.println("Argument " + arg + " is not understandable, ignoring");
+                continue;
+            }
+            if (Configuration.properties.getProperty(match.group(1)) != null) {
+                Configuration.properties.setProperty(match.group(1), match.group(2));
+            } else {
+                System.out.println("Wrong argument " + arg + ", ignoring");
+            }
+        }
     }
 }
--- src/com/swen90004/rebellion/Simulation.java
+++ src/com/swen90004/rebellion/Simulation.java
@@ -2,50 +2,13 @@ package com.swen90004.rebellion;
 
 import java.util.List;
 import java.util.Random;
-import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Simulation {
     public static Random random = new Random();
 
-    /** Read command line and override any configuration values.
-     *
-     * @param args The args array passed to main()
-     */
-    public static void parseCmdLineArgs(String[] args) {
-        for (String arg: args) {
-            if (arg.equals("--help") || arg.equals("-h")) {
-                System.out.println("Here are the default values of the program:");
-                Set<String> keys = Configuration.properties.stringPropertyNames();
-                for (String key : keys) {
-                    System.out.println("  " + key + " = " + Configuration.properties.getProperty(key));
-                }
-                System.out.println("It can be executed with different values for example:");
-                System.out.println("java Simulation --k=2.5 --vision=5");
-                System.exit(0);
-            }
-        }
-
-        // Expect a series of --x=y arguments
-        Pattern pattern = Pattern.compile("^--(.+)=(.*)$");
-        for (String arg : args) {
-            Matcher match = pattern.matcher(arg);
-            if (!match.matches()) {
-                System.out.println("Argument " + arg + " is not understandable, ignoring");
-                continue;
-            }
-            if (Configuration.properties.getProperty(match.group(1)) != null) {
-                Configuration.properties.setProperty(match.group(1), match.group(2));
-            } else {
-                System.out.println("Wrong argument " + arg + ", ignoring");
-            }
-        }
-    }
-
     public static void main(String[] args) {
         Configuration.loadConfiguration();
-        parseCmdLineArgs(args);
+        Configuration.parseCmdLineArgs(args);
 
 	    Map map = new Map(Configuration.getInt("boardSize"));
 	    map.initialiseBoard();
